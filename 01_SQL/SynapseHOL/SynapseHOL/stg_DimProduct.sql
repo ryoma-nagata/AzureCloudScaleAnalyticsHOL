78ab9196f9bfba36f83b116e08db731029d8476e
亮磨 永田 <ryoma.nagata@avantcorp.com>
2020-08-28 20:12:22 +0900

    add stg

---
+++ 01_SQL/SynapseHOL/SynapseHOL/stg_DimProduct.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE [dbo].[stg_DimProduct]
+(
+    [ProductKey] [int] NOT NULL,
+    [ProductLabel] [nvarchar](255) NULL,
+    [ProductName] [nvarchar](500) NULL,
+    [ProductDescription] [nvarchar](400) NULL,
+    [ProductSubcategoryKey] [int] NULL,
+    [Manufacturer] [nvarchar](50) NULL,
+    [BrandName] [nvarchar](50) NULL,
+    [ClassID] [nvarchar](10) NULL,
+    [ClassName] [nvarchar](20) NULL,
+    [StyleID] [nvarchar](10) NULL,
+    [StyleName] [nvarchar](20) NULL,
+    [ColorID] [nvarchar](10) NULL,
+    [ColorName] [nvarchar](20) NOT NULL,
+    [Size] [nvarchar](50) NULL,
+    [SizeRange] [nvarchar](50) NULL,
+    [SizeUnitMeasureID] [nvarchar](20) NULL,
+    [Weight] [float] NULL,
+    [WeightUnitMeasureID] [nvarchar](20) NULL,
+    [UnitOfMeasureID] [nvarchar](10) NULL,
+    [UnitOfMeasureName] [nvarchar](40) NULL,
+    [StockTypeID] [nvarchar](10) NULL,
+    [StockTypeName] [nvarchar](40) NULL,
+    [UnitCost] [money] NULL,
+    [UnitPrice] [money] NULL,
+    [AvailableForSaleDate] [datetime] NULL,
+    [StopSaleDate] [datetime] NULL,
+    [Status] [nvarchar](7) NULL,
+    [ImageURL] [nvarchar](150) NULL,
+    [ProductURL] [nvarchar](150) NULL,
+    [ETLLoadID] [int] NULL,
+    [LoadDate] [datetime] NULL,
+    [UpdateDate] [datetime] NULL
+)
+WITH
+(
+    DISTRIBUTION = HASH (ProductKey),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
---
+++ 01_SQL/SynapseHOL/SynapseHOL/stg_FactOnlineSales.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[stg_FactOnlineSales]
+(
+    [OnlineSalesKey] [int]  NOT NULL,
+    [DateKey] [datetime] NOT NULL,
+    [StoreKey] [int] NOT NULL,
+    [ProductKey] [int] NOT NULL,
+    [PromotionKey] [int] NOT NULL,
+    [CurrencyKey] [int] NOT NULL,
+    [CustomerKey] [int] NOT NULL,
+    [SalesOrderNumber] [nvarchar](20) NOT NULL,
+    [SalesOrderLineNumber] [int] NULL,
+    [SalesQuantity] [int] NOT NULL,
+    [SalesAmount] [money] NOT NULL,
+    [ReturnQuantity] [int] NOT NULL,
+    [ReturnAmount] [money] NULL,
+    [DiscountQuantity] [int] NULL,
+    [DiscountAmount] [money] NULL,
+    [TotalCost] [money] NOT NULL,
+    [UnitCost] [money] NULL,
+    [UnitPrice] [money] NULL,
+    [ETLLoadID] [int] NULL,
+    [LoadDate] [datetime] NULL,
+    [UpdateDate] [datetime] NULL
+)
+WITH
+(
+    DISTRIBUTION = HASH ([ProductKey]),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
