e8f1f8b791d45214faf1e90ef3539216da427fd8
亮磨 永田 <ryoma.nagata@avantcorp.com>
2020-08-28 20:41:07 +0900

    fix

---
+++ 01_SQL/SynapseHOL/SynapseHOL/dbo/StoredProcedures/usp_Update_DimProduct.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC [dbo].[usp_Update_DimProduct]
+AS
+BEGIN
+	
+	IF OBJECT_ID(N'new_DimProduct') IS NOT NULL
+    BEGIN
+	    DROP TABLE  [new_DimProduct]
+	END
+	
+	CREATE TABLE [dbo].[new_DimProduct]            
+	WITH (DISTRIBUTION = HASH([ProductKey]  ) ) AS 
+	SELECT * FROM [dbo].[stg_DimProduct] OPTION (LABEL = 'CTAS :  [dbo].[new_DimProduct]');
+
+    IF OBJECT_ID(N'old_DimProduct') IS NOT NULL
+    BEGIN
+	    DROP TABLE  [old_DimProduct]
+	END
+
+    RENAME OBJECT dbo.[DimProduct]      TO [old_DimProduct];
+	RENAME OBJECT dbo.[new_DimProduct]  TO [DimProduct];
+
+END
---
+++ 01_SQL/SynapseHOL/SynapseHOL/dbo/StoredProcedures/usp_Update_FactOnlineSales.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC [dbo].[usp_Update_FactOnlineSales]
+AS
+BEGIN
+	
+	IF OBJECT_ID(N'new_DimProduct') IS NOT NULL
+    BEGIN
+	    DROP TABLE  [new_FactOnlineSales]
+	END
+	
+	CREATE TABLE [dbo].[new_FactOnlineSales]            
+	WITH (DISTRIBUTION = HASH([ProductKey]  ) ) AS 
+	SELECT * FROM [dbo].[stg_FactOnlineSales] OPTION (LABEL = 'CTAS :  [dbo].[new_DimProduct]');
+
+    IF OBJECT_ID(N'old_DimProduct') IS NOT NULL
+    BEGIN
+	    DROP TABLE  [old_FactOnlineSales]
+	END
+
+    RENAME OBJECT dbo.[FactOnlineSales]      TO [old_FactOnlineSales];
+	RENAME OBJECT dbo.[new_FactOnlineSales]  TO [FactOnlineSales];
+
+END
