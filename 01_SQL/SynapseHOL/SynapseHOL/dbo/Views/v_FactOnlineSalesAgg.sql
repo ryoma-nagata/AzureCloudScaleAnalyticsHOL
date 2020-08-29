d45f205ad40a26832be7187739dffa0506f90e90
亮磨 永田 <ryoma.nagata@avantcorp.com>
2020-08-29 12:16:09 +0900

    add view

---
+++ 01_SQL/SynapseHOL/SynapseHOL/dbo/Views/v_FactOnlineSalesAgg.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[v_FactOnlineSalesAgg]
+	AS 
+	SELECT 
+		DateKey
+		,ProductKey
+		,SUM(SalesQuantity) SalesQuantity
+		,SUM(SalesAmount) SalesAmount
+		,SUM(TotalCost) TotalCost
+	FROM [FactOnlineSales]
+	GROUP BY
+		DateKey
+		,ProductKey
